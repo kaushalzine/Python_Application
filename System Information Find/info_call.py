1bed54571f20e458878529525e5cd4a0d740e9ce
KAUSHAL ASHOK ZINE <32410264+kaushalzine@users.noreply.github.com>
2020-02-21 12:11:57 +0530

    Add files via upload

---
+++ System Information Find/info_call.py
@@ -0,0 +1,13 @@
+from system_info_module import *;
+
+print(CPU_Info_OS())
+print(Paltform_Info())
+print(get_size(10000)) 
+# size check 
+
+print(Boot_Info())
+
+print(CPU_Info())
+
+print(RAM_Usage())
+print(Disk_Info())
